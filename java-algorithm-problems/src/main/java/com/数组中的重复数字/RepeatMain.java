a77c2d50f951863b1d23a2c16de4685350ba0965
taoheng <zongzr@shtianxin.com>
2019-05-06 10:28:20 +0800

    ...

---
+++ java-algorithm-problems/src/main/java/com/数组中的重复数字/RepeatMain.java
@@ -0,0 +1,58 @@
+package com.数组中的重复数字;
+
+import com.bbz.util.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * com.数组中的重复数字
+ * <p>
+ * Created by tianxin2 on 2019-05-05
+ */
+public class RepeatMain {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 2, 3, 4, 3};
+
+        duplicate(nums, nums.length, nums);
+
+        System.out.println(Arrays.toString(nums));
+
+
+    }
+
+
+    /**
+     * todo 待优化
+     * @param nums
+     * @param length
+     * @param duplication
+     * @return
+     */
+    public static boolean duplicate(int[] nums, int length, int[] duplication) {
+        if (nums == null || length <= 0) {
+            return false;
+        }
+        for (int i = 0; i < length; i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    duplication[i] = nums[i];
+                    return true;
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return false;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+}
