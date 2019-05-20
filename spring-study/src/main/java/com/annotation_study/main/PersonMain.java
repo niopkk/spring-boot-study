b4649d20923d540e2f99baaa7efa0f9b31810616
taoheng <zongzr@shtianxin.com>
2019-05-20 17:13:40 +0800

    ...

--- spring-study/src/main/java/com/annotation_study/main/PersonMain.java
+++ spring-study/src/main/java/com/annotation_study/main/PersonMain.java
@@ -5,6 +5,8 @@ import com.annotation_study.pojo.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class PersonMain {
 
 
@@ -21,7 +23,24 @@ public class PersonMain {
         System.out.println(beanName);
 
         for (String s : beanNamesForType) {
-            System.out.println("beanNamesForType:"+s);
+            System.out.println("beanNamesForType:" + s);
+        }
+        int[] ints = {1, 42, 3, 2, 89, 4, 34};
+        sort(ints);
+        System.out.println(Arrays.toString(ints));
+    }
+
+
+    private static void sort(int... array) {
+        int len = array.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
         }
     }
 }
