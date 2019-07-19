208b6ad4a19bbfd65015180feec1be4735ead710
taoheng <zongzr@shtianxin.com>
2019-07-19 09:47:35 +0800

    ..

--- java-effective/src/main/java/com/bbz/builder/NutrilonFactsMainTest.java
+++ java-effective/src/main/java/com/bbz/builder/NutrilonFactsMainTest.java
@@ -1,5 +1,12 @@
 package com.bbz.builder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class NutrilonFactsMainTest {
 
     public static void main(String[] args) {
@@ -9,5 +16,23 @@ public class NutrilonFactsMainTest {
 
 
         System.out.printf(cocaColal.toString());
+
+        Map<String, String> maps = new HashMap<>();
+        maps.put("k", "123");
+        maps.put("a", "323");
+        List<Map<String, String>> list = new ArrayList<>();
+        list.add(maps);
+
+
+        final List<Map<String, String>> collect = list.stream().map(e ->
+                e.entrySet().stream()
+                        .filter(map -> map.getValue() == "123")
+                        .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()))
+        ).collect(Collectors.toList());
+
+        System.out.println(collect);
+
+
+
     }
 }
