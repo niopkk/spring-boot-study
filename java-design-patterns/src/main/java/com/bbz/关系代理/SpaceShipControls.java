cb3c02f8df2697b5744af81b002ba4ee5435e983
TH <781686379@qq.com>
2019-09-28 09:42:10 +0800

    关系代理

---
+++ java-design-patterns/src/main/java/com/bbz/关系代理/SpaceShipControls.java
@@ -0,0 +1,14 @@
+package com.bbz.关系代理;
+
+public class SpaceShipControls {
+
+
+
+    void up(int velocity){
+        System.out.printf("up");
+    }
+
+    void down(int velocity){}
+
+    void left(int velocity){}
+}
