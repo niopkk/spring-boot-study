41d6409939919253766ec29f257c3b588409093d
taoheng <zongzr@shtianxin.com>
2020-01-03 14:13:19 +0800

    ..

--- spirng-springmvc/src/main/java/com/bbz/SpittrWebAppInitializer.java
+++ spirng-springmvc/src/main/java/com/bbz/SpittrWebAppInitializer.java
@@ -8,21 +8,21 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  * com.bbz
  * <p>
  */
-public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{RootConfig.class};
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
-}
+//public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class<?>[]{RootConfig.class};
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[]{WebConfig.class};
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
+//}
