bdf30dc5060fbca903cab71acaaf19a6b1a1bf72
taoheng <zongzr@shtianxin.com>
2020-04-20 14:33:54 +0800

    ..

---
+++ java-thread/src/main/java/com/threadpooldemo/ThreadPoolDemo.java
@@ -0,0 +1,51 @@
+package com.threadpooldemo;
+
+import java.util.concurrent.*;
+
+/**
+ * com.threadpooldemo
+ * <p>
+ * Created by tianxin2 on 2020/1/14
+ */
+public class ThreadPoolDemo {
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":Thread Id: " + Thread.currentThread().getId());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+//        ExecutorService exec = Executors.newFixedThreadPool(5);
+//
+//
+//        for (int i = 0; i < 10; i++) {
+//            exec.submit(task);
+//        }
+        final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            final ScheduledFuture<String> schedule = scheduledExecutorService.schedule(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return "dsdfsfdfd";
+                }
+            }, 1, TimeUnit.SECONDS);
+            try {
+                System.out.println(schedule.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
