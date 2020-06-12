be9817dba776822be45e3087e242630fe45ee794
taoheng <zongzr@shtianxin.com>
2020-06-12 14:52:12 +0800

    ..

--- spring-boot-mongo/src/main/java/com/bbz/Application.java
+++ spring-boot-mongo/src/main/java/com/bbz/Application.java
@@ -1,12 +1,28 @@
 package com.bbz;
 
+import com.bbz.model.Student;
+import com.bbz.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private StudentService studentService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @RequestMapping("/save")
+    public String save() {
+        studentService.save(new Student("dsd", "测试11111", 33333333));
+        return "save";
     }
 }
--- spring-boot-mongo/src/main/java/com/bbz/service/impl/StudentServiceImpl.java
+++ spring-boot-mongo/src/main/java/com/bbz/service/impl/StudentServiceImpl.java
@@ -1,20 +1,25 @@
 package com.bbz.service.impl;
 
 import com.bbz.model.Student;
+import com.bbz.repository.StudentRepository;
 import com.bbz.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentServiceImpl implements StudentService {
 
 
-    @Autowired
-    private MongoOperations mongoOperations;
+//    @Autowired
+//    private MongoOperations mongoOperations;
 
+    @Autowired
+    private StudentRepository studentRepository;
     @Override
     public Student save(Student student) {
 
-
+        studentRepository.save(student);
         return null;
 
     }
