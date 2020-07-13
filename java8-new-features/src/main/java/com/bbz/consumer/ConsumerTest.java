7eb6524fc49653aab5c6aeba55b990ed8837a545
taoheng <zongzr@shtianxin.com>
2020-07-13 13:50:01 +0800

    ..

--- java8-new-features/src/main/java/com/bbz/consumer/ConsumerTest.java
+++ java8-new-features/src/main/java/com/bbz/consumer/ConsumerTest.java
@@ -8,22 +8,25 @@ import java.util.function.Consumer;
 public class ConsumerTest {
 
 
-    static <T> T of(T value, Consumer<T>... callbacks) {
+    static <T> T of(T value, Consumer<T> callbacks) {
         if (Objects.nonNull((value))) {
-            for (Consumer<T> callback : callbacks) {
-                callback.accept(value);
-            }
+            callbacks.accept(value);
             return value;
         }
         return null;
     }
 
     public static void main(String[] args) {
-        Test t = of(new Test(),
-                test -> test.setId("1"),
-                test -> test.setName("张三"));
-        System.out.println(Jsons.toJson(t));
+        Test t1 = new Test();
+
+        Test t3=  of(t1,
+                test -> test.setId("1"));
+        System.out.println(Jsons.toJson(t3));
+        Test t4 = of(t1,test -> test.setName("1"));
+
+        System.out.println(Jsons.toJson(t4));
     }
+
     static class Test {
         private String id;
 
@@ -32,12 +35,15 @@ public class ConsumerTest {
         public String getId() {
             return id;
         }
+
         public void setId(String id) {
             this.id = id;
         }
+
         public String getName() {
             return name;
         }
+
         public void setName(String name) {
             this.name = name;
         }
