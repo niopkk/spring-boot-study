f6ae201c0da219218a941411fbead2e1aeeff4ad
taoheng <zongzr@shtianxin.com>
2020-07-17 17:50:18 +0800

    ..

--- spring-boot-security/src/main/java/com/bbz/config/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/bbz/config/SecurityConfig.java
@@ -3,26 +3,24 @@ package com.bbz.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.inMemoryAuthentication()
-                .withUser("zhangsan").password("123").authorities("/p1").roles("ADMIN")
+                .withUser("zhangsan").password("123").authorities("/r/p1","/r/p2")
                 .and()
-                .withUser("lis").password("123").authorities("/p2").roles("ADMIN");
+                .withUser("lis").password("123").authorities("/r/p2");
     }
 
     @Bean
@@ -36,8 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/r/**").authenticated()
-                .anyRequest().permitAll()
+//                .antMatchers("/r/**").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+//               .successForwardUrl("")
                 .and()
                 .csrf()
                 .disable();
--- spring-boot-security/src/main/java/com/bbz/controller/HomeController.java
+++ spring-boot-security/src/main/java/com/bbz/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bbz.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 
-    @RequestMapping(value = {"/r/p1"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/r/r1"}, method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('/r/p1')")//拥有p1权限才可以访问
     public String index() {
 
         return "张三拥有p1权限";
     }
 
-    @RequestMapping(value = {"/r/p2"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/r/r2"}, method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('/r/p2')")//拥有p1权限才可以访问
     public String p2() {
 
         return "lis拥有p2权限";
     }
-    @RequestMapping(value = {"/p3"}, method = RequestMethod.GET)
+
+    @RequestMapping(value = {"/r3"}, method = RequestMethod.GET)
     public String p3() {
 
         return "lis拥有p3权限";
