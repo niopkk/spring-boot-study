e218a079939e03feea979ddfc6f36c0e8ef7e93e
taoheng <zongzr@shtianxin.com>
2020-08-04 16:50:21 +0800

    ..

--- java-thread/src/main/java/com/控制调用远程每秒的次数/Test.java
+++ java-thread/src/main/java/com/控制调用远程每秒的次数/Test.java
@@ -7,11 +7,13 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class Test {
+
+    //    http://ifeve.com/concurrency-practice-1/#more-4019
     final static int MAX_QPS = 100;
 
     final static Semaphore semaphore = new Semaphore(MAX_QPS);
 
-    public static void main (String ... args) throws Exception {
+    public static void main(String... args) throws Exception {
 
         Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
 
@@ -19,7 +21,7 @@ public class Test {
 
             public void run() {
 
-                semaphore.release(MAX_QPS/2);
+                semaphore.release(MAX_QPS / 2);
 
             }
 
@@ -28,32 +30,36 @@ public class Test {
         //lots of concurrent calls:100 * 1000
         ExecutorService pool = Executors.newFixedThreadPool(10);
 
-        for (int i=10;i>0;i--) {
+        for (int i = 10; i > 0; i--) {
 
             final int x = i;
 
             pool.submit(() -> {
 
-                for (int j=1000;j>0;j--) {
+//                for (int j=6;j>0;j--) {
 
+                while (true) {
                     semaphore.acquireUninterruptibly(1);
-                    remoteCall(x, j);
+
+                    remoteCall(x, 1);
 
                 }
+
+//                }
 
             });
 
         }
 
-        pool.shutdown();
+//        pool.shutdown();
+//
+//        pool.awaitTermination(1, TimeUnit.HOURS);
 
-        pool.awaitTermination(1, TimeUnit.HOURS);
-
-        System.out.println("DONE");
+//        System.out.println("DONE");
     }
 
     private static void remoteCall(int i, int j) {
-        System.out.println(String.format("%s - %s: %d %d",new Date(),
+        System.out.println(String.format("%s ,%s- %s: %d %d", System.currentTimeMillis(), new Date(),
                 Thread.currentThread(), i, j));
     }
 
