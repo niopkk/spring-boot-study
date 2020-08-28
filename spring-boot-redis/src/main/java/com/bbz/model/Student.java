22d51acb9051fbc85ee9e61000a25976a8f919e3
taoheng <zongzr@shtianxin.com>
2020-08-28 17:59:24 +0800

    ..

---
+++ spring-boot-redis/src/main/java/com/bbz/model/Student.java
@@ -0,0 +1,17 @@
+package com.bbz.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    private String id;
+
+    private String name;
+}
